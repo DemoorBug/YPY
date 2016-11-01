1f6e2fa7836bb1fdaab0b58e3c2d959ed2a2910b
DemoorBug <1004041672@qq.com>
2016-11-01 11:11:24 +0800

    oker

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ gulp.task('default',function() {
     var cssFilter = filter('**/*.css', {restore: true});
     var indexHtmlFilter = filter(['**/*','!**/*.html'], {restore: true});
 
-    return gulp.src('src/index.html')
+    return gulp.src('src/*.html')
         .pipe(useref())
         .pipe(jsFilter)
         .pipe(uglify())
